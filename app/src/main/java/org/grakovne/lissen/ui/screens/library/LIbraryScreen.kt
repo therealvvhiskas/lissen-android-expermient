5f76a09e97565766b4b96ee721c959e7e4c21c0a
grakovne <grakovne@gmail.com>
2024-09-21 01:26:41 +0200

    library screen

--- app/src/main/java/org/grakovne/lissen/ui/screens/library/LIbraryScreen.kt
+++ app/src/main/java/org/grakovne/lissen/ui/screens/library/LIbraryScreen.kt
@@ -23,17 +23,26 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Cloud
 import androidx.compose.material.icons.outlined.CloudDownload
+import androidx.compose.material.icons.outlined.Pause
+import androidx.compose.material.icons.outlined.PauseCircle
+import androidx.compose.material.icons.outlined.PlayArrow
+import androidx.compose.material.icons.outlined.PlayCircle
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
@@ -69,6 +78,9 @@ fun LibraryScreen(
             } else {
                 Spacer(modifier = Modifier.height(24.dp))
             }
+        },
+        bottomBar = {
+            MiniPlayer()
         },
         modifier = Modifier
             .systemBarsPadding()
@@ -237,6 +249,72 @@ fun BookItem(book: Book) {
                 text = "12h 24m",
                 style = MaterialTheme.typography.bodySmall
             )
+        }
+    }
+}
+
+@Composable
+fun MiniPlayer(
+    modifier: Modifier = Modifier,
+    currentBook: Book = Book(
+        title = "What Does Fox Says?",
+        author = "John Show",
+        downloaded = true
+    )
+) {
+    var isPlaying: Boolean by remember { mutableStateOf(false) }
+
+    Surface(
+        color = Color.Transparent,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            Image(
+                painter = painterResource(id = R.drawable.fallback_cover),
+                contentDescription = "Current Book Cover",
+                modifier = Modifier
+                    .size(52.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+                contentScale = ContentScale.Crop
+            )
+
+            Spacer(modifier = Modifier.width(12.dp))
+
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = currentBook.title,
+                    style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = currentBook.author,
+                    style = MaterialTheme.typography.bodySmall.copy(
+                        color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.6f)
+                    ),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+
+            IconButton(
+                onClick = { isPlaying = !isPlaying }
+            ) {
+                Icon(
+                    imageVector = if (isPlaying) Icons.Outlined.PauseCircle else Icons.Outlined.PlayCircle,
+                    contentDescription = if (isPlaying) "Pause" else "Play"
+                )
+            }
         }
     }
 }
