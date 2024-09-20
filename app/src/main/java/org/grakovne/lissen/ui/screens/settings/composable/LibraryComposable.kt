41ebd14eb3369f4019121541bcfe8825363275ca
grakovne <grakovne@gmail.com>
2024-09-20 02:52:48 +0200

    navigation

--- app/src/main/java/org/grakovne/lissen/ui/screens/settings/composable/LibraryComposable.kt
+++ app/src/main/java/org/grakovne/lissen/ui/screens/settings/composable/LibraryComposable.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowDropDown
 import androidx.compose.material.icons.outlined.ArrowDropUp
@@ -23,15 +24,23 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import org.grakovne.lissen.ui.screens.settings.Library
 import org.grakovne.lissen.viewmodel.ConnectionViewModel
 
 @Composable
 fun LibraryComposable(viewModel: ConnectionViewModel) {
     val isServerConnected by viewModel.isConnected.observeAsState(false)
-    val libraries by viewModel.libraries.observeAsState(emptyList())
-    val preferredLibrary by viewModel.preferredLibrary.observeAsState()
+    val libraries by viewModel.libraries.observeAsState(
+        listOf(
+            Library("id", "Sci-Hub"),
+            Library("id", "Fiction"),
+            Library("id", "Tales"),
+        )
+    )
+    val preferredLibrary by viewModel.preferredLibrary.observeAsState(Library("id", "Sci-Hub"))
 
     var expanded by remember { mutableStateOf(false) }
 
@@ -48,7 +57,7 @@ fun LibraryComposable(viewModel: ConnectionViewModel) {
                 modifier = Modifier.fillMaxWidth(),
                 enabled = isServerConnected
             ) {
-                Text(preferredLibrary?.title ?: "No Library Selected")
+                Text(preferredLibrary.title)
                 Spacer(modifier = Modifier.weight(1f))
                 Icon(
                     imageVector = if (expanded) Icons.Outlined.ArrowDropUp else Icons.Outlined.ArrowDropDown,
@@ -58,7 +67,8 @@ fun LibraryComposable(viewModel: ConnectionViewModel) {
 
             DropdownMenu(
                 expanded = expanded,
-                modifier = Modifier.background(color = colorScheme.background),
+                modifier = Modifier
+                    .background(color = colorScheme.background),
                 onDismissRequest = { expanded = false }
             ) {
                 libraries
@@ -71,7 +81,7 @@ fun LibraryComposable(viewModel: ConnectionViewModel) {
                                 }
                                 expanded = false
                             },
-                            enabled = isServerConnected
+                            enabled = true
                         )
                     }
             }
--- app/src/main/java/org/grakovne/lissen/ui/screens/settings/composable/ServerComposable.kt
+++ app/src/main/java/org/grakovne/lissen/ui/screens/settings/composable/ServerComposable.kt
@@ -1,19 +1,16 @@
 package org.grakovne.lissen.ui.screens.settings.composable
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Visibility
-import androidx.compose.material.icons.filled.VisibilityOff
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Podcasts
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -23,9 +20,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import org.grakovne.lissen.viewmodel.ConnectionViewModel
 
@@ -37,66 +34,121 @@ fun ServerComposable(viewModel: ConnectionViewModel) {
     var password by remember { mutableStateOf("") }
     var passwordVisibility: Boolean by remember { mutableStateOf(false) }
 
-    Column(modifier = Modifier.padding(16.dp)) {
+    Column() {
         Text(
             text = "Server Connection",
             style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold),
-            modifier = Modifier.padding(bottom = 12.dp)
+            modifier = Modifier.padding(start = 16.dp, bottom = 12.dp)
         )
 
-        OutlinedTextField(
-            value = serverUrl,
-            onValueChange = { viewModel.updateServerUrl(it) },
-            label = { Text("Server URL") },
-            shape = RoundedCornerShape(16.dp),
-            singleLine = true,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 4.dp)
-        )
+        ListItem(
+            headlineContent = {
+                Row(
+                    modifier = Modifier
 
-        OutlinedTextField(
-            value = login,
-            onValueChange = { viewModel.updateLogin(it) },
-            label = { Text("Login") },
-            shape = RoundedCornerShape(16.dp),
-            singleLine = true,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 4.dp)
-        )
-
-        OutlinedTextField(
-            value = password,
-            visualTransformation = if (!passwordVisibility) PasswordVisualTransformation() else VisualTransformation.None,
-            onValueChange = { password = it },
-            trailingIcon = {
-                IconButton(onClick = { passwordVisibility = !passwordVisibility }) {
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
                     Icon(
-                        imageVector = if (passwordVisibility) Icons.Filled.VisibilityOff else Icons.Filled.Visibility,
-                        contentDescription = "Show Password"
+                        Icons.Outlined.Podcasts,
+                        contentDescription = "Preferences",
+                        modifier = Modifier
+                            .padding(start = 10.dp, end = 10.dp)
+                            .size(24.dp)
                     )
+                    Column(
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+
+                            Text(
+                                modifier = Modifier.padding(start = 10.dp),
+                                text = "audiobook.grakovne.org",
+                                style = TextStyle(fontFamily = FontFamily.Monospace)
+                            )
+                        }
+                        Text(
+                            modifier = Modifier.padding(start = 10.dp, top = 4.dp),
+                            text = "2.13.4",
+                            style = TextStyle(fontFamily = FontFamily.Monospace)
+                        )
+                    }
                 }
             },
-            label = { Text("Password") },
-            shape = RoundedCornerShape(16.dp),
-            singleLine = true,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(vertical = 4.dp)
+            trailingContent = {
+                Icon(
+                    Icons.Outlined.Delete,
+                    contentDescription = "Close",
+                    modifier = Modifier.size(24.dp)
+                )
+            }
         )
 
-        Spacer(modifier = Modifier.height(16.dp))
+//
+//        Button(
+//            onClick = {  },
+//            colors = ButtonDefaults.buttonColors(Color.Gray)
+//        ) {
+//            Text(text = "Disconnect")
+//        }
 
-        Button(
-            onClick = {},
-            modifier = Modifier
-                .fillMaxWidth(0.5f)
-                .padding(top = 4.dp)
-                .align(Alignment.CenterHorizontally),
-            shape = RoundedCornerShape(16.dp)
-        ) {
-            Text(text = "Connect")
-        }
+
+//        OutlinedTextField(
+//            value = serverUrl,
+//            onValueChange = { viewModel.updateServerUrl(it) },
+//            label = { Text("Server URL") },
+//            shape = RoundedCornerShape(16.dp),
+//            singleLine = true,
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .padding(vertical = 4.dp)
+//        )
+//
+//        OutlinedTextField(
+//            value = login,
+//            onValueChange = { viewModel.updateLogin(it) },
+//            label = { Text("Login") },
+//            shape = RoundedCornerShape(16.dp),
+//            singleLine = true,
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .padding(vertical = 4.dp)
+//        )
+//
+//        OutlinedTextField(
+//            value = password,
+//            visualTransformation = if (!passwordVisibility) PasswordVisualTransformation() else VisualTransformation.None,
+//            onValueChange = { password = it },
+//            trailingIcon = {
+//                IconButton(onClick = { passwordVisibility = !passwordVisibility }) {
+//                    Icon(
+//                        imageVector = if (passwordVisibility) Icons.Filled.VisibilityOff else Icons.Filled.Visibility,
+//                        contentDescription = "Show Password"
+//                    )
+//                }
+//            },
+//            label = { Text("Password") },
+//            shape = RoundedCornerShape(16.dp),
+//            singleLine = true,
+//            modifier = Modifier
+//                .fillMaxWidth()
+//                .padding(vertical = 4.dp)
+//        )
+
+//        Spacer(modifier = Modifier.height(16.dp))
+
+//
+//        Button(
+//            onClick = {},
+//            modifier = Modifier
+//                .fillMaxWidth(0.5f)
+//                .padding(top = 4.dp)
+//                .align(Alignment.CenterHorizontally),
+//            shape = RoundedCornerShape(16.dp)
+//        ) {
+//            Text(text = "Disconnect")
+//        }
     }
 }
