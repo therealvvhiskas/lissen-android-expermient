75f29e42b07a3683c8d2a1b1db1d3273dd32c172
grakovne <grakovne@gmail.com>
2024-09-30 01:04:30 +0200

    library screen

--- app/src/main/java/org/grakovne/lissen/ui/screens/library/LIbraryScreen.kt
+++ app/src/main/java/org/grakovne/lissen/ui/screens/library/LIbraryScreen.kt
@@ -16,7 +16,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.MoreVert
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -30,7 +34,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -77,6 +80,14 @@ fun LibraryScreen(
     Scaffold(
         topBar = {
             TopAppBar(
+                actions = {
+                    IconButton(onClick = { }) {
+                        Icon(
+                            imageVector = Icons.Outlined.MoreVert,
+                            contentDescription = "Menu"
+                        )
+                    }
+                },
                 title = {
                     Crossfade(targetState = navBarTitle, label = "navbar_title_fade") { title ->
                         Text(
