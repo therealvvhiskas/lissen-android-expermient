f4ddfa570cad22676341599af8e0420018727af5
grakovne <grakovne@gmail.com>
2024-09-30 01:56:01 +0200

    library screen

--- app/src/main/java/org/grakovne/lissen/ui/screens/library/LIbraryScreen.kt
+++ app/src/main/java/org/grakovne/lissen/ui/screens/library/LIbraryScreen.kt
@@ -9,19 +9,31 @@ import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.BugReport
+import androidx.compose.material.icons.outlined.Code
+import androidx.compose.material.icons.outlined.Download
+import androidx.compose.material.icons.outlined.Feedback
+import androidx.compose.material.icons.outlined.Flag
+import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material.icons.outlined.MoreVert
+import androidx.compose.material.icons.outlined.RateReview
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material.icons.outlined.StarRate
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -36,11 +48,16 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import dagger.hilt.android.EntryPointAccessors
@@ -96,27 +113,73 @@ fun LibraryScreen(
                         )
                     }
                     DropdownMenu(
-                        modifier = Modifier.background(color = colorScheme.background),
                         expanded = expanded,
-                        onDismissRequest = { expanded = false }
+                        onDismissRequest = { expanded = false },
+                        modifier = Modifier
+                            .background(colorScheme.surface)
+                            .padding(8.dp)
                     ) {
                         DropdownMenuItem(
-                            text = { Text("Downloads") },
-                            onClick = {
-                                expanded = false
-                            }
+                            leadingIcon = {
+                                Icon(
+                                    modifier = Modifier.alpha(0.6f),
+                                    imageVector = Icons.Outlined.Download,
+                                    contentDescription = null,
+                                )
+                            },
+                            text = {
+                                Text(
+                                    "Downloads",
+                                    style = MaterialTheme.typography.bodyLarge,
+                                    modifier = Modifier.padding(start = 8.dp).alpha(0.6f)
+                                )
+                            },
+                            onClick = { expanded = false },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 4.dp)
                         )
                         DropdownMenuItem(
-                            text = { Text("Preferences") },
+                            leadingIcon = {
+                                Icon(
+                                    imageVector = Icons.Outlined.Settings,
+                                    contentDescription = null,
+                                )
+                            },
+                            text = {
+                                Text(
+                                    "Preferences",
+                                    style = MaterialTheme.typography.bodyLarge,
+                                    modifier = Modifier.padding(start = 8.dp)
+                                )
+                            },
                             onClick = {
                                 expanded = false
-                            }
+                                navController.navigate("settings_screen")
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 4.dp)
                         )
                         DropdownMenuItem(
-                            text = { Text("Send feedback") },
-                            onClick = {
-                                expanded = false
-                            }
+                            leadingIcon = {
+                                Icon(
+                                    modifier = Modifier.alpha(0.6f),
+                                    imageVector = Icons.Outlined.Flag,
+                                    contentDescription = null,
+                                )
+                            },
+                            text = {
+                                Text(
+                                    "Report Issue",
+                                    style = MaterialTheme.typography.bodyLarge,
+                                    modifier = Modifier.padding(start = 8.dp).alpha(0.6f)
+                                )
+                            },
+                            onClick = { expanded = false },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 4.dp)
                         )
                     }
                 },
@@ -132,7 +195,7 @@ fun LibraryScreen(
             )
         },
         bottomBar = {
-            MiniPlayerComposable(navController)
+            //MiniPlayerComposable(navController)
         },
         modifier = Modifier
             .systemBarsPadding()
