cfc6be29c3e8f3cd7c920103627e705913ca9e81
grakovne <grakovne@gmail.com>
2024-09-29 23:34:21 +0200

    library screen

--- app/src/main/java/org/grakovne/lissen/ui/theme/Theme.kt
+++ app/src/main/java/org/grakovne/lissen/ui/theme/Theme.kt
@@ -3,6 +3,8 @@ package org.grakovne.lissen.ui.theme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val LightColorScheme = lightColorScheme(
     primary = FoxOrange,
@@ -15,10 +17,24 @@ private val LightColorScheme = lightColorScheme(
 fun LissenTheme(
     content: @Composable () -> Unit
 ) {
+    val systemUiController = rememberSystemUiController()
+    val backgroundColor = MaterialTheme.colorScheme.background
+
+    SideEffect {
+        systemUiController.setNavigationBarColor(
+            color = backgroundColor,
+            darkIcons = false
+        )
+
+        systemUiController.setStatusBarColor(
+            color = backgroundColor,
+            darkIcons = true
+        )
+    }
+
     MaterialTheme(
         colorScheme = LightColorScheme,
         typography = Typography,
         content = content
     )
-
 }
