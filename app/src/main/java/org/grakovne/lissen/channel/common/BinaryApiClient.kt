c100df048083d09fecff09c9d785727b555ccceb
grakovne <grakovne@gmail.com>
2024-10-12 01:19:29 +0200

    polish

--- app/src/main/java/org/grakovne/lissen/channel/common/BinaryApiClient.kt
+++ app/src/main/java/org/grakovne/lissen/channel/common/BinaryApiClient.kt
@@ -8,7 +8,6 @@ import retrofit2.Retrofit
 import java.io.File
 import java.util.concurrent.TimeUnit
 
-
 class BinaryApiClient(
     host: String,
     token: String,
@@ -17,36 +16,25 @@ class BinaryApiClient(
 
     private val cacheSize: Long = 128 * 1024 * 1024
 
-    private val loggingInterceptor = HttpLoggingInterceptor().apply {
-        level = HttpLoggingInterceptor.Level.BASIC
-    }
-
     private val httpClient = OkHttpClient.Builder()
         .cache(Cache(cacheDir, cacheSize))
-        .addInterceptor(loggingInterceptor)
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BASIC
+        })
+        .addInterceptor { chain: Interceptor.Chain ->
+            val request = chain
+                .request()
+                .newBuilder()
+                .header("Authorization", "Bearer $token")
+                .build()
+            chain.proceed(request)
+        }
+        .connectTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
         .build()
 
     val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(host)
         .client(httpClient)
-        .client(secureClient(token))
         .build()
-
-    companion object {
-        fun secureClient(token: String): OkHttpClient {
-            return OkHttpClient.Builder()
-                .addInterceptor { chain: Interceptor.Chain ->
-                    val request = chain
-                        .request()
-                        .newBuilder()
-                        .header("Authorization", "Bearer $token")
-                        .build()
-
-                    chain.proceed(request)
-                }
-                .connectTimeout(30, TimeUnit.SECONDS)
-                .readTimeout(30, TimeUnit.SECONDS)
-                .build()
-        }
-    }
 }
