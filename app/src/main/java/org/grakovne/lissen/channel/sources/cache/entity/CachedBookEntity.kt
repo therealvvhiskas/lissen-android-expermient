2eeee0f7322107ff2b6297c1e52fbeb175ef8a11
grakovne <grakovne@gmail.com>
2024-10-18 02:36:59 +0200

    offline

---
+++ app/src/main/java/org/grakovne/lissen/channel/sources/cache/entity/CachedBookEntity.kt
@@ -0,0 +1,91 @@
+package org.grakovne.lissen.channel.sources.cache.entity
+
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+import androidx.room.Relation
+import java.io.Serializable
+
+data class CachedBookEntity(
+    @Embedded val detailedBook: BookEntity,
+
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "bookId"
+    )
+    val files: List<BookFile>,
+
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "bookId"
+    )
+    val chapters: List<BookChapter>,
+
+    @Relation(
+        parentColumn = "id",
+        entityColumn = "bookId"
+    )
+    val progress: MediaProgress?
+)
+
+
+@Entity(tableName = "detailed_books")
+data class BookEntity(
+    @PrimaryKey val id: String,
+    val title: String,
+    val author: String,
+    val duration: Int,
+) : Serializable
+
+@Entity(
+    tableName = "book_files",
+    foreignKeys = [ForeignKey(
+        entity = BookEntity::class,
+        parentColumns = ["id"],
+        childColumns = ["bookId"],
+        onDelete = ForeignKey.CASCADE
+    )]
+)
+data class BookFile(
+    @PrimaryKey val id: String,
+    val name: String,
+    val duration: Double,
+    val mimeType: String,
+    val bookId: String
+) : Serializable
+
+@Entity(
+    tableName = "book_chapters",
+    foreignKeys = [ForeignKey(
+        entity = BookEntity::class,
+        parentColumns = ["id"],
+        childColumns = ["bookId"],
+        onDelete = ForeignKey.CASCADE
+    )]
+)
+data class BookChapter(
+    @PrimaryKey val id: String,
+    val duration: Double,
+    val start: Double,
+    val end: Double,
+    val title: String,
+    val bookId: String
+) : Serializable
+
+@Entity(
+    tableName = "media_progress",
+    foreignKeys = [ForeignKey(
+        entity = BookEntity::class,
+        parentColumns = ["id"],
+        childColumns = ["bookId"],
+        onDelete = ForeignKey.CASCADE
+    )]
+)
+data class MediaProgress(
+    @PrimaryKey(autoGenerate = true) val id: Long,
+    val bookId: String,
+    val currentTime: Double,
+    val isFinished: Boolean,
+    val lastUpdate: Long
+) : Serializable
---
+++ app/src/main/java/org/grakovne/lissen/channel/sources/cache/entity/PlaybackProgressEntity.kt
@@ -0,0 +1,11 @@
+package org.grakovne.lissen.channel.sources.cache.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "playback_progress")
+data class PlaybackProgressEntity(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val currentTime: Double,
+    val totalTime: Double
+)
