266e2f444f720ced04361142ba7a4e2516c034ae
grakovne <grakovne@gmail.com>
2024-10-22 00:24:41 +0200

    UI polish

--- app/src/main/java/org/grakovne/lissen/ui/screens/player/composable/TrackControlComposable.kt
+++ app/src/main/java/org/grakovne/lissen/ui/screens/player/composable/TrackControlComposable.kt
@@ -1,5 +1,7 @@
 package org.grakovne.lissen.ui.screens.player.composable
 
+//noinspection UsingMaterialAndMaterial3Libraries
+//noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -7,12 +9,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-//noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.Slider
-//noinspection UsingMaterialAndMaterial3Libraries
 import androidx.compose.material.SliderDefaults
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Forward30
+import androidx.compose.material.icons.outlined.Forward30
 import androidx.compose.material.icons.rounded.PauseCircleFilled
 import androidx.compose.material.icons.rounded.PlayCircleFilled
 import androidx.compose.material.icons.rounded.Replay30
@@ -20,14 +20,13 @@ import androidx.compose.material.icons.rounded.SkipNext
 import androidx.compose.material.icons.rounded.SkipPrevious
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableDoubleStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -43,7 +42,6 @@ fun TrackControlComposable(
     modifier: Modifier = Modifier
 ) {
     val isPlaying by viewModel.isPlaying.observeAsState(false)
-
     val playbackReady by viewModel.isPlaybackReady.observeAsState(false)
     val currentTrackIndex by viewModel.currentChapterIndex.observeAsState(0)
     val currentTrackPosition by viewModel.currentChapterPosition.observeAsState(0.0)
@@ -52,7 +50,7 @@ fun TrackControlComposable(
     val book by viewModel.book.observeAsState()
     val chapters = book?.chapters ?: emptyList()
 
-    var sliderPosition by remember { mutableDoubleStateOf(0.0) }
+    var sliderPosition by remember { mutableStateOf(0.0) }
     var isDragging by remember { mutableStateOf(false) }
 
     LaunchedEffect(currentTrackPosition, currentTrackIndex, currentTrackDuration) {
@@ -69,115 +67,128 @@ fun TrackControlComposable(
     }
 
     Column(
-        modifier = modifier.padding(horizontal = 16.dp)
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
     ) {
-
-        Slider(
-            value = sliderPosition.toFloat(),
-            onValueChange = { newPosition ->
-                isDragging = true
-                sliderPosition = newPosition.toDouble()
-            },
-            onValueChangeFinished = {
-                isDragging = false
-                viewModel.seekTo(sliderPosition)
-            },
-            valueRange = 0f..currentTrackDuration.toFloat(),
-            colors = SliderDefaults
-                .colors(
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Slider(
+                value = sliderPosition.toFloat(),
+                onValueChange = { newPosition ->
+                    isDragging = true
+                    sliderPosition = newPosition.toDouble()
+                },
+                onValueChangeFinished = {
+                    isDragging = false
+                    viewModel.seekTo(sliderPosition)
+                },
+                valueRange = 0f..currentTrackDuration.toFloat(),
+                colors = SliderDefaults.colors(
                     thumbColor = colorScheme.primary,
                     activeTrackColor = colorScheme.primary
+                ),
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 4.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = currentTrackPosition.toInt().formatFully(),
+                    style = typography.bodySmall,
+                    color = colorScheme.onBackground.copy(alpha = 0.6f)
                 )
-        )
+                Text(
+                    text = "-${
+                        maxOf(0.0, currentTrackDuration - currentTrackPosition)
+                            .toInt()
+                            .formatFully()
+                    }",
+                    style = typography.bodySmall,
+                    color = colorScheme.onBackground.copy(alpha = 0.6f)
+                )
+            }
+        }
 
         Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 4.dp, vertical = 2.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(
-                text = currentTrackPosition.toInt().formatFully(),
-                style = MaterialTheme.typography.bodySmall,
-                color = colorScheme.onBackground.copy(alpha = 0.6f)
-            )
-            Text(
-                text = "-${
-                    maxOf(0.0, currentTrackDuration - currentTrackPosition).toInt().formatFully()
-                }",
-                style = MaterialTheme.typography.bodySmall,
-                color = colorScheme.onBackground.copy(alpha = 0.6f)
-            )
-        }
-    }
-
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        IconButton(
-            onClick = {
-                if (currentTrackIndex > 0) {
-                    viewModel.previousTrack()
-                }
+            IconButton(
+                onClick = {
+                    if (currentTrackIndex > 0) {
+                        viewModel.previousTrack()
+                    }
+                },
+                enabled = currentTrackIndex > 0
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.SkipPrevious,
+                    contentDescription = "Previous Track",
+                    tint = if (currentTrackIndex > 0) colorScheme.onBackground else colorScheme.onBackground.copy(
+                        alpha = 0.3f
+                    ),
+                    modifier = Modifier.size(36.dp)
+                )
             }
-        ) {
-            Icon(
-                imageVector = Icons.Rounded.SkipPrevious,
-                contentDescription = "Previous",
-                tint = colorScheme.onBackground,
-                modifier = Modifier.size(36.dp)
-            )
-        }
 
-        IconButton(
-            onClick = { viewModel.rewind() },
-        ) {
-            Icon(
-                imageVector = Icons.Rounded.Replay30,
-                contentDescription = "Replay",
-                tint = colorScheme.onBackground,
-                modifier = Modifier.size(48.dp)
-            )
-        }
-
-        IconButton(
-            onClick = { viewModel.togglePlayPause() },
-            modifier = Modifier.size(72.dp)
-        ) {
-            Icon(
-                imageVector = if (isPlaying) Icons.Rounded.PauseCircleFilled else Icons.Rounded.PlayCircleFilled,
-                contentDescription = "Play / Pause toggle",
-                tint = colorScheme.primary,
-                modifier = Modifier.fillMaxSize()
-            )
-        }
-
-        IconButton(
-            onClick = { viewModel.forward() }
-        ) {
-            Icon(
-                imageVector = Icons.Default.Forward30,
-                contentDescription = "Forward",
-                tint = colorScheme.onBackground,
-                modifier = Modifier.size(48.dp)
-            )
-        }
-
-        IconButton(
-            onClick = {
-                if (currentTrackIndex < chapters.size.minus(1)) {
-                    viewModel.nextTrack()
-                }
+            IconButton(
+                onClick = { viewModel.rewind() },
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.Replay30,
+                    contentDescription = "Rewind",
+                    tint = colorScheme.onBackground,
+                    modifier = Modifier.size(48.dp)
+                )
             }
-        ) {
-            Icon(
-                imageVector = Icons.Rounded.SkipNext,
-                contentDescription = "Next Track",
-                tint = colorScheme.onBackground,
-                modifier = Modifier.size(36.dp),
-            )
+
+            IconButton(
+                onClick = { viewModel.togglePlayPause() },
+                modifier = Modifier.size(72.dp)
+            ) {
+                Icon(
+                    imageVector = if (isPlaying) Icons.Rounded.PauseCircleFilled else Icons.Rounded.PlayCircleFilled,
+                    contentDescription = "Play / Pause",
+                    tint = colorScheme.primary,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
+
+            IconButton(
+                onClick = { viewModel.forward() }
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Forward30,
+                    contentDescription = "Forward",
+                    tint = colorScheme.onBackground,
+                    modifier = Modifier.size(48.dp)
+                )
+            }
+
+            IconButton(
+                onClick = {
+                    if (currentTrackIndex < chapters.size - 1) {
+                        viewModel.nextTrack()
+                    }
+                },
+                enabled = currentTrackIndex < chapters.size - 1
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.SkipNext,
+                    contentDescription = "Next Track",
+                    tint = if (currentTrackIndex < chapters.size - 1) colorScheme.onBackground else colorScheme.onBackground.copy(
+                        alpha = 0.3f
+                    ),
+                    modifier = Modifier.size(36.dp),
+                )
+            }
         }
     }
 }
