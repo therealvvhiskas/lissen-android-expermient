7e3f34b6e0e2e09f763a095f2a15200ee2d380bd
Max Grakov <grakovne@gmail.com>
2025-04-03 19:38:35 +0200

    wip (#140)

--- app/src/main/java/org/grakovne/lissen/playback/MediaModule.kt
+++ app/src/main/java/org/grakovne/lissen/playback/MediaModule.kt
@@ -3,24 +3,30 @@ package org.grakovne.lissen.playback
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import android.util.Log
 import androidx.annotation.OptIn
 import androidx.media3.common.AudioAttributes
 import androidx.media3.common.C
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.CommandButton
 import androidx.media3.session.MediaSession
+import androidx.media3.session.SessionCommand
+import androidx.media3.session.SessionResult
+import com.google.common.util.concurrent.ListenableFuture
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import org.grakovne.lissen.ui.activity.AppActivity
+import org.grakovne.lissen.widget.MediaRepository
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object MediaModule {
-
     @OptIn(UnstableApi::class)
     @Provides
     @Singleton
@@ -43,6 +49,7 @@ object MediaModule {
     @Singleton
     fun provideMediaSession(
         @ApplicationContext context: Context,
+        mediaRepository: MediaRepository,
         exoPlayer: ExoPlayer,
     ): MediaSession {
         val sessionActivityPendingIntent = PendingIntent.getActivity(
@@ -53,7 +60,65 @@ object MediaModule {
         )
 
         return MediaSession.Builder(context, exoPlayer)
+            .setCallback(object : MediaSession.Callback {
+
+                @OptIn(UnstableApi::class)
+                override fun onConnect(
+                    session: MediaSession,
+                    controller: MediaSession.ControllerInfo,
+                ): MediaSession.ConnectionResult {
+                    val rewindCommand = SessionCommand(REWIND_COMMAND, Bundle.EMPTY)
+                    val forwardCommand = SessionCommand(FORWARD_COMMAND, Bundle.EMPTY)
+
+                    val sessionCommands = MediaSession.ConnectionResult.DEFAULT_SESSION_COMMANDS.buildUpon()
+                        .add(rewindCommand)
+                        .add(forwardCommand)
+                        .build()
+
+                    val rewindButton = CommandButton
+                        .Builder(CommandButton.ICON_SKIP_BACK_10)
+                        .setSessionCommand(rewindCommand)
+                        .setDisplayName("Rewind")
+                        .setEnabled(true)
+                        .build()
+
+                    val forwardButton = CommandButton
+                        .Builder(CommandButton.ICON_SKIP_FORWARD_30)
+                        .setSessionCommand(forwardCommand)
+                        .setDisplayName("Forward")
+                        .setEnabled(true)
+                        .build()
+
+                    return MediaSession
+                        .ConnectionResult
+                        .AcceptedResultBuilder(session)
+                        .setAvailableSessionCommands(sessionCommands)
+                        .setCustomLayout(listOf(rewindButton, forwardButton))
+                        .build()
+                }
+
+                override fun onCustomCommand(
+                    session: MediaSession,
+                    controller: MediaSession.ControllerInfo,
+                    customCommand: SessionCommand,
+                    args: Bundle,
+                ): ListenableFuture<SessionResult> {
+                    Log.d(TAG, "Executing: ${customCommand.customAction}")
+
+                    when (customCommand.customAction) {
+                        FORWARD_COMMAND -> mediaRepository.forward()
+                        REWIND_COMMAND -> mediaRepository.rewind()
+                    }
+
+                    return super.onCustomCommand(session, controller, customCommand, args)
+                }
+            })
             .setSessionActivity(sessionActivityPendingIntent)
             .build()
     }
+
+    private const val REWIND_COMMAND = "notification_rewind"
+    private const val FORWARD_COMMAND = "notification_forward"
+
+    private const val TAG = "MediaModule"
 }
