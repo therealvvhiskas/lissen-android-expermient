ce0e3d751a4073969472a33a745887a8492e2e85
grakovne <grakovne@gmail.com>
2025-04-10 02:52:58 +0200

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         applicationId = "org.grakovne.lissen"
         minSdk = 28
         targetSdk = 35
-        versionCode = 90
-        versionName = "1.3.12"
+        versionCode = 91
+        versionName = "1.3.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
