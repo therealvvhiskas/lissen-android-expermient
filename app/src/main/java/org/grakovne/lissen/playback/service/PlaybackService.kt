ec57bb9854d87e4521dcb45a96951d8fa2019b4e
therealvvhiskas <151652479+therealvvhiskas@users.noreply.github.com>
2025-06-25 16:21:21 +0200

    Update PlaybackService.kt

--- app/src/main/java/org/grakovne/lissen/playback/service/PlaybackService.kt
+++ app/src/main/java/org/grakovne/lissen/playback/service/PlaybackService.kt
@@ -1,18 +1,39 @@
 package org.grakovne.lissen.playback.service
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.content.Context
-import android.os.Build
+import android.content.Intent
 import android.os.Handler
 import android.os.Looper
-import androidx.core.app.NotificationCompat
+import android.util.Log
+import androidx.annotation.OptIn
+import androidx.core.net.toUri
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import androidx.media3.common.MediaItem
+import androidx.media3.common.MediaMetadata
 import androidx.media3.common.util.UnstableApi
+import androidx.media3.datasource.DefaultDataSource
+import androidx.media3.datasource.okhttp.OkHttpDataSource
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.ProgressiveMediaSource
+import androidx.media3.exoplayer.source.SilenceMediaSource
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
 import dagger.hilt.android.AndroidEntryPoint
-import org.grakovne.lissen.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.grakovne.lissen.LissenApplication
+import org.grakovne.lissen.channel.audiobookshelf.common.api.RequestHeadersProvider
+import org.grakovne.lissen.common.createOkHttpClient
+import org.grakovne.lissen.content.LissenMediaProvider
+import org.grakovne.lissen.domain.BookFile
+import org.grakovne.lissen.domain.DetailedItem
+import org.grakovne.lissen.domain.MediaProgress
+import org.grakovne.lissen.persistence.preferences.LissenSharedPreferences
+import java.io.File
 import javax.inject.Inject
 
 @AndroidEntryPoint
